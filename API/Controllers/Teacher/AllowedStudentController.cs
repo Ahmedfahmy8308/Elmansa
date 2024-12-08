7533b7ec372b382b0ba226c933ef1e796bef7ff4
Ahmed Fahmy <63644503+Ahmedfahmy8308@users.noreply.github.com>
2024-12-08 20:00:21 +0200

    add First migration

---
+++ API/Controllers/Teacher/AllowedStudentController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers.Teacher
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllowedStudentController : ControllerBase
+    {
+
+        readonly AppDbContext _mydb;
+        public AllowedStudentController(AppDbContext mydb)
+        {
+            _mydb = mydb;
+        }
+
+        [HttpGet]
+        public async Task<List<AllowedStudent>> Get()
+        {
+            var AllowedStudents = await _mydb.AllowedStudents.ToListAsync();
+
+            return AllowedStudents;
+        }
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -21,13 +21,13 @@ namespace API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var context = builder.Services.BuildServiceProvider().GetService<AppDbContext>();
+            //var context = builder.Services.BuildServiceProvider().GetService<AppDbContext>();
             //context.Database.EnsureCreated();
             //context.Database.Migrate();
             //context.Database.EnsureDeleted();
 
-            var sql = context.Database.GenerateCreateScript();
-            Console.WriteLine(sql);
+            //var sql = context.Database.GenerateCreateScript();
+            //Console.WriteLine(sql);
 
             var app = builder.Build();
 
@@ -44,6 +44,7 @@ namespace API
             app.MapControllers();
 
             app.Run();
+
         }
     }
 }
