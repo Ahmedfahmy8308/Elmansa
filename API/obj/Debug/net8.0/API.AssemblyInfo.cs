e315367c04e3cc7b247e942c0e68b43b863620b8
Ahmed Fahmy <63644503+Ahmedfahmy8308@users.noreply.github.com>
2024-12-08 20:40:09 +0200

    New ERD

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f4d1d5ac58e02ad0d92604c31af7436326cfd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f1bf86d031b053f024d9d3b10e80d17c813bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
