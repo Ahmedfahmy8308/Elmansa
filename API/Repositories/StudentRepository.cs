24a5b08bffd78298e94e2ff873c2ce7412a8dd12
Ahmed Fahmy <63644503+Ahmedfahmy8308@users.noreply.github.com>
2024-12-16 06:12:50 +0200

    add login

---
+++ API/Repositories/StudentRepository.cs
@@ -0,0 +1,25 @@
+﻿using API.Entities;
+using API.Repositories;
+
+namespace API.Data.Repositories
+{
+    public class StudentRepository : GenericRepository<Student> , IStudentRepository
+    {
+        readonly AppDbContext _context;
+        public StudentRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public bool IsSsnExit(string ssn)
+        {
+            return _context.Students.Any(s => s.SSN == ssn) ;
+        }
+
+        public bool IsStudentAllowed(string Email)
+        {
+            var allowedStudent = _context.AllowedStudents.Find(Email);
+            return allowedStudent != null && allowedStudent.AccountActive;
+        }
+    }
+}
---
+++ API/UOW/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using API.Data.Repositories;
+using API.Entities;
+using API.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace API.UOW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IStudentRepository Students { get; }
+        IRepository<AllowedStudent> AllowedStudents { get; }
+
+        Task<int> CompleteAsync();
+    }
+}
