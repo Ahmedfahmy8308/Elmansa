f19edd5a10b347907bde4c8ba2f473226f73a314
Ahmed Fahmy <63644503+Ahmedfahmy8308@users.noreply.github.com>
2024-12-21 20:57:21 +0200

    Update GroupController.cs

--- API/Controllers/TeacherControllers/GroupController.cs
+++ API/Controllers/TeacherControllers/GroupController.cs
@@ -4,6 +4,7 @@ using API.DTO.Teacher;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API.UOW;
 
 namespace API.Controllers.TeacherControllers
 {
@@ -11,10 +12,10 @@ namespace API.Controllers.TeacherControllers
     [ApiController]
     public class GroupController : ControllerBase
     {
-        readonly AppDbContext _mydb;
-        public GroupController(AppDbContext mydb)
+        readonly IUnitOfWork _unitOfWork;
+        public GroupController(IUnitOfWork unitOfWork)
         {
-            _mydb = mydb;
+            _unitOfWork = unitOfWork; 
         }
 
 
@@ -22,7 +23,7 @@ namespace API.Controllers.TeacherControllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var Opj = await _mydb.Groups.ToListAsync();
+            var Opj = await _unitOfWork.Groups.GetAllAsync();
             var Groups = new List<Group_dto>();
 
             foreach (var item in Opj)
@@ -39,6 +40,25 @@ namespace API.Controllers.TeacherControllers
             return Ok(Groups);
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var Opj = await _unitOfWork.Groups.GetByIdAsync(id);
+            if (Opj == null)
+            {
+                return NotFound("No Such group ");
+            }
+            Group_dto group = new Group_dto();
+            group.Group_id = Opj.ID;
+            group.Group_name = Opj.Name;
+            group.Groub_stage = Opj.Stage;
+            group.Group_stage_level = Opj.StageLevel;
+            group.Group_created_data = Opj.CreatedAt;
+            group.Group_active = Opj.IsActive;
+            return Ok(group);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post(Group_dto group)
         {
@@ -49,8 +69,8 @@ namespace API.Controllers.TeacherControllers
             Opj.CreatedAt = group.Group_created_data;
             Opj.IsActive = group.Group_active;
 
-            await _mydb.Groups.AddAsync(Opj);
-            await _mydb.SaveChangesAsync();
+            await _unitOfWork.Groups.AddAsync(Opj);
+            await _unitOfWork.CompleteAsync();
             return Ok(group);
         }
 
@@ -58,20 +78,20 @@ namespace API.Controllers.TeacherControllers
         [HttpDelete]
         public async Task<IActionResult> Delete(Group_dto group )
         {
-            var Opj = await _mydb.Groups.FirstOrDefaultAsync(x => x.ID == group.Group_id);
+            var Opj = await _unitOfWork.Groups.GetByIdAsync(group.Group_id);
             if (Opj == null)
             {
                 return NotFound("No Such group ");
             }
-            _mydb.Groups.Remove(Opj);
-            await _mydb.SaveChangesAsync();
+            _unitOfWork.Groups.Delete(Opj);
+            await _unitOfWork.CompleteAsync();
             return Ok();
         }
 
         [HttpPut]
         public async Task<IActionResult> Put(Group_dto group)
         {
-            var Opj = await _mydb.Groups.FirstOrDefaultAsync(x => x.ID == group.Group_id);
+            var Opj = await _unitOfWork.Groups.GetByIdAsync(group.Group_id);
             if (Opj == null)
             {
                 return NotFound("No Such group ");
@@ -82,7 +102,8 @@ namespace API.Controllers.TeacherControllers
             Opj.CreatedAt = group.Group_created_data;
             Opj.IsActive = group.Group_active;
 
-            await _mydb.SaveChangesAsync();
+            _unitOfWork.Groups.Update(Opj);
+            await _unitOfWork.CompleteAsync();
             return Ok(group);
         }
 
